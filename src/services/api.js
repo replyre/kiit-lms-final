8f616b10a93a975e7b69230eaed0df8f462a17d0
Rahul <replyrgupta@gmail.com>
2025-07-23 22:28:50 +0530

    added old backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://itc-backend-peach.vercel.app/api";
+const API_URL = "https://education-system-delta.vercel.app/api";
 
 const api = axios.create({
   baseURL: API_URL,
